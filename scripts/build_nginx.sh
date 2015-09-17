9bdc158d1f4d2ebe0330f05160cdf240716d7248
Ashu <ashutosh.abhinav@target.com>
2015-09-17 14:08:04 -0500

    Syncing the module versions

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,9 +9,9 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.5.7}
-PCRE_VERSION=${PCRE_VERSION-8.21}
-HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
+NGINX_VERSION=${NGINX_VERSION-1.7.12}
+PCRE_VERSION=${PCRE_VERSION-8.36}
+HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.25}
 
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
